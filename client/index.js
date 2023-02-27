170fdacffaa0ea12e2293ea0ee47209213f6ab46
nabmat <up2111821@myport.ac.uk>
2023-02-27 14:12:07 +0000

    javascript happy meter decreases based on others

--- client/index.js
+++ client/index.js
@@ -14,6 +14,7 @@ const pet = {
   happiness: 0,
 };
 
+// const petValues = pet.cleanliness + pet.hungerness + pet.sleepiness;
 
 function init() {
   const nameButton = document.querySelector('#setName');
@@ -27,47 +28,65 @@ function init() {
   pet.hungerness = 100;
   window.setInterval(adjustHunger, 100);
   const hungerFeedElem = document.querySelector('#hungerfeed');
-  hungerFeedElem.addEventListener('click', play);
+  hungerFeedElem.addEventListener('click', feed);
 
   pet.cleanliness = 100;
   window.setInterval(adjustClean, 100);
   const cleaningElem = document.querySelector('#cleaning');
   cleaningElem.addEventListener('click', playingClean);
 
+
   pet.happiness = 100;
-  window.setInterval(adjustHappy, 100);
-  const happyIncElem = document.querySelector('#happiness');
-  happyIncElem.addEventListener('click', playHappy);
+  // window.setInterval(updateHappyMeter(), 100);
+  // const happyIncElem = document.querySelector('#happiness');
+  // happyIncElem.addEventListener('click', playHappy);
 }
 
 // Sleep bar
 function playing() {
   pet.sleepiness = 100;
+  pet.happiness = 100;
 }
 
 function adjustSleep() {
   pet.sleepiness = pet.sleepiness - 0.1;
+  pet.happiness = pet.happiness - 0.1;
   if (pet.sleepiness < 0) {
     pet.sleepiness = 0;
   }
+  if (pet.happiness < 0) {
+    pet.happiness = 0;
+  }
   updateSleepMeter();
+  updateHappyMeter();
 }
-
 function updateSleepMeter() {
   const sleepElem = document.querySelector('#sleep');
   sleepElem.value = pet.sleepiness;
 }
 
 // Hunger bar
-function play() {
+function feed() {
   pet.hungerness = 100;
+  pet.happiness = 100;
 }
+
+function playingClean() {
+  pet.cleanliness = 100;
+  pet.happiness = 100;
+}
+
 function adjustHunger() {
   pet.hungerness = pet.hungerness - 0.25;
+  pet.happiness = pet.happiness - 0.1;
   if (pet.hungerness < 0) {
     pet.hungerness = 0;
   }
+  if (pet.happiness < 0) {
+    pet.happiness = 0;
+  }
   updateHungerMeter();
+  updateHappyMeter();
 }
 
 function updateHungerMeter() {
@@ -76,22 +95,17 @@ function updateHungerMeter() {
 }
 
 
-init();
-
-
-// Clean bar
-
-
-function playingClean() {
-  pet.cleanliness = 100;
-}
-
 function adjustClean() {
   pet.cleanliness = pet.cleanliness - 0.1;
+  pet.happiness = pet.happiness - 0.1;
   if (pet.cleanliness < 0) {
     pet.cleanliness = 0;
   }
+  if (pet.happiness < 0) {
+    pet.happiness = 0;
+  }
   adjustCleanMeters();
+  updateHappyMeter();
 }
 
 function adjustCleanMeters() {
@@ -100,17 +114,10 @@ function adjustCleanMeters() {
 }
 
 
-function playHappy() {
-  pet.happiness = 100;
-}
-function adjustHappy() {
-  pet.happiness = pet.happiness - 0.25;
-  if (pet.happiness < 0) {
-    pet.happiness = 0;
-  }
-  updateHappyMeter();
-}
 function updateHappyMeter() {
   const happyElem = document.querySelector('#happy');
   happyElem.value = pet.happiness;
 }
+
+
+init();
