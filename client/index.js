b178f28df2bf504e9c9720a9c1b65214286fd6f0
nabmat <up2111821@myport.ac.uk>
2023-03-30 16:13:35 +0100

    values of meters saved to localstorage

--- client/index.js
+++ client/index.js
@@ -1,7 +1,10 @@
 'use strict';
 let start = false;
 
-
+/** Displays the name entered and saves it in the local storage.
+ * Also displays it in HTML in this case.
+ * Displays an error message if name has not been entered.
+ */
 function nameHandler() {
   const nameInputElem = document.querySelector('#userText');
   const newName = nameInputElem.value;
@@ -10,22 +13,30 @@ function nameHandler() {
     // display error message
   } else {
     start = true;
-    localStorage.setItem('nameValue', newName); // if pet stored, this redundant
+    pet = newPet();
+    pet.name = newName;
+    localStorage.setItem('myPet', JSON.stringify(pet));
+    window.setInterval(savePet, 1000);
+  }
+  if (pet.happiness === null) {
+    originalValues();
   }
   updateName(newName);
 }
 
-let pet = {
-  hungerness: 0,
-  sleepiness: 0,
-  cleanliness: 0,
-  happiness: 0,
-};
+/** Creates a new pet. All attributes are set to 0. */
+function newPet() {
+  return {
+    hungerness: 100,
+    sleepiness: 100,
+    cleanliness: 100,
+    happiness: 100,
+  };
+}
 
 let timer = 0; // should be param of pet
 let petAlive;
 let checkInput;
-let savePetTimer;
 
 
 function updateName(newName) {
@@ -51,33 +62,43 @@ function adjustTimer() {
 
 function removeName() {
   updateName('');
-  localStorage.removeItem('nameValue'); // this too is redundant
   refreshPage();
 }
 
 function refreshPage() {
   window.location.reload();
+  localStorage.clear();
 }
 
 
 function init() {
+
   const refresh = document.querySelector('#clearName');
   refresh.addEventListener('click', refreshPage);
-
   // get the name, display it, hide the name inputs
-  checkInput = setInterval(checkStart, 1000);
-  const nameButton = document.querySelector('#setName');
-  nameButton.addEventListener('click', nameHandler);
-  if (localStorage.getItem('nameValue')) { // would be replaced with pet beint stored
+
+  if (JSON.parse(localStorage.getItem('myPet'))) { // would be replaced with pet beint stored
     const nameElem1 = document.querySelector('#petName');
-    nameElem1.textContent = localStorage.getItem('nameValue');
+    pet = JSON.parse(localStorage.getItem('myPet'));
+    nameElem1.textContent = pet.name;
+    startPet();
+
+    window.setInterval(savePet, 1000);
+  } else {
+    checkInput = setInterval(checkStart, 1000);
+    const nameButton = document.querySelector('#setName');
+    nameButton.addEventListener('click', nameHandler);
   }
 
-  savePetTimer = setInterval(savePet, 10000);
   // this should be every 10 seconds
 }
 
-let savePet = localStorage.setItem('myPet', JSON.stringify(pet));
+
+function savePet() {
+  localStorage.setItem('myPet', JSON.stringify(pet));
+}
+let pet = JSON.parse(localStorage.getItem('myPet')) ?? newPet();
+
 
 function checkStart() {
   if (start === true) {
@@ -88,43 +109,28 @@ function checkStart() {
 }
 
 function startPet() {
-  // check if pet exists in local storage
-  pet = JSON.parse(localStorage.getItem('myPet'));
-  console.log(pet);
-  if (savePet === false) {
+  if (pet.happiness === null) {
     originalValues();
-  } else {
-    savePet = true;
   }
-  // if (savePet === false) {
-  //   originalValues();
-  // } else {
-  //   savePet = true;
-  // }
-
-
-  pet.sleepiness = 100;
+  // pet.sleepiness = 100;
   window.setInterval(decreaseSleep, 100);
   const sleepingElem = document.querySelector('#sleeping');
   sleepingElem.addEventListener('click', playing);
 
-  pet.hungerness = 100;
+  // pet.hungerness = 100;
   window.setInterval(decreaseHunger, 100);
   const hungerFeedElem = document.querySelector('#hungerfeed');
   hungerFeedElem.addEventListener('click', feed);
 
-  pet.cleanliness = 100;
+  // pet.cleanliness = 100;
   window.setInterval(decreaseClean, 100);
   const cleaningElem = document.querySelector('#cleaning');
   cleaningElem.addEventListener('click', playingClean);
 
-  pet.happiness = 100;
-
-
+  // pet.happiness = 100;
   const removeNameElem = document.querySelector('#clearName');
   removeNameElem.addEventListener('click', removeName);
 }
-
 // Sleep bar
 function playing() {
   pet.sleepiness = 100;
@@ -207,9 +213,11 @@ function updateHappyMeter() {
 
 function deathBoth() {
   if (pet.hungerness === 0 && pet.sleepiness === 0) {
+    localStorage.clear();
     const petStatusBoth = document.querySelector('#petStatus');
     petStatusBoth.textContent = 'Your pet has died due to hunger and lack of sleep, please refresh the page to continue playing';
   } else if (pet.hungerness === 0) {
+    localStorage.clear();
     document.querySelector('#hungerfeed').disabled = true;
     document.querySelector('#sleeping').disabled = true;
     document.querySelector('#cleaning').disabled = true;
@@ -218,6 +226,7 @@ function deathBoth() {
     const petStatusHunger = document.querySelector('#petStatus');
     petStatusHunger.textContent = 'Your pet has died due to starvation, please refresh the page to continue playing';
   } else if (pet.sleepiness === 0) {
+    localStorage.clear();
     document.querySelector('#sleeping').disabled = true;
     document.querySelector('#hungerfeed').disabled = true;
     document.querySelector('#cleaning').disabled = true;
@@ -225,7 +234,7 @@ function deathBoth() {
     pet.happiness = 0;
     const petStatusSleep = document.querySelector('#petStatus');
     petStatusSleep.textContent = 'Your pet has died due to lack of sleep, please refresh the page to continue playing';
-    clearInterval(savePetTimer);
+    clearInterval(savePet);
   }
 }
 
