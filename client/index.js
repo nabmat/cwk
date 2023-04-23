87a1b3f864caafb61c08239c6a86e237920e6c90
nabmat <up2111821@myport.ac.uk>
2023-04-23 19:25:19 +0100

    added comments so user knows what function is for

--- client/index.js
+++ client/index.js
@@ -27,10 +27,11 @@ function nameHandler() {
   }
   updateName(newName);
 }
-/** Hides input box and enter element once user types and enters in name */
+/** Hides input box and enter button once user types and enters in name */
 function hideElem() {
   document.querySelector('#setName').style.visibility = 'hidden';
   document.querySelector('#userText').style.visibility = 'hidden';
+  saveuserInput();
 }
 /** Creates a new pet. All attributes are set to 100. */
 function newPet() {
@@ -42,23 +43,29 @@ function newPet() {
   };
 }
 
-let timer = 0; // should be param of pet
+
+/** Variables that called for different functions  */
+let timer = 0;
 let petAlive;
 let checkInput;
 let score = 0;
 
+
+/** Adds 1 to the scoreboard each time the user interacts with the pet through the buttons */
 function updateScore() {
   score++;
   const scoreValue = document.querySelector('#scoreValue');
   scoreValue.textContent = score + ' ';
+  saveScoreValue();
   // Just need to work on localstorage
 }
-
+/** Displays the users petname back to them replacing the header 1 tag */
 function updateName(newName) {
   const nameElem = document.querySelector('#petName');
   nameElem.textContent = newName;
 }
 
+/** If the pet does not have any values set to it then this function will start the pet off with full attributes */
 function originalValues() {
   pet.happiness = 100;
   pet.sleepiness = 100;
@@ -66,6 +73,7 @@ function originalValues() {
   pet.hungerness = 100;
 }
 
+/** If the pets hunger and or the sleep level is 0 then stop the timer  */
 function adjustTimer() {
   timer++;
   if (pet.hungerness === 0 || pet.sleepiness === 0) {
@@ -74,24 +82,43 @@ function adjustTimer() {
   }
 }
 
+/** LocalStorage function which sets the input elements to remain hidden despite the uesr refreshing */
+function saveuserInput() {
+  localStorage.setItem('hideInput', true);
+}
+function saveScoreValue() {
+  localStorage.setItem('savingScore', score);
+}
 
+/** This function is called when the user hits reset pet it will get rid of the name and call the refreshPage function */
 function removeName() {
   updateName('');
   refreshPage();
 }
 
+/** This function is called when the user hits reset pet, the game will restart by getting rid of all previous meter values */
 function refreshPage() {
   window.location.reload();
   localStorage.clear();
 }
 
-
+/** Allows the user to reset the pet causing them to restart
+ * Saves the pet's name, attributes to localStorage every 1 second
+ * if the user clicks enter and there is a name in the input field then it runs the nameHandler function which displays the name to the user
+ */
 function init() {
   const refresh = document.querySelector('#resetPet');
   refresh.addEventListener('click', refreshPage);
-  // get the name, display it, hide the name inputs
-
-  if (JSON.parse(localStorage.getItem('myPet'))) { // would be replaced with pet beint stored
+  const hideInput = localStorage.getItem('hideInput');
+  if (hideInput) {
+    hideElem();
+  }
+  const scoreValues = (localStorage.getItem('savingScore'));
+  if (scoreValues) {
+    score = JSON.parse(scoreValues);
+    updateScore();
+  }
+  if (JSON.parse(localStorage.getItem('myPet'))) {
     const nameElem1 = document.querySelector('#petName');
     pet = JSON.parse(localStorage.getItem('myPet'));
     nameElem1.textContent = pet.name;
@@ -105,13 +132,17 @@ function init() {
   }
 }
 
-
+/** Stores the pet object to localstorage by converting it into a JSON string
+ * Gets the function of newpet from localstorage by parsing the JSON string back into a javascript object
+*/
 function savePet() {
   localStorage.setItem('myPet', JSON.stringify(pet));
 }
 let pet = JSON.parse(localStorage.getItem('myPet')) ?? newPet();
 
-
+/** Checks if the boolean is true then stop checkign for user input as its already entered
+ * Start the pet and set a timer which will count down till the pets death
+  */
 function checkStart() {
   if (start === true) {
     clearInterval(checkInput);
@@ -119,7 +150,11 @@ function checkStart() {
     petAlive = setInterval(adjustTimer, 1000);
   }
 }
-
+/** If the pet does not have any values then the function gives it values starting from 100
+ * Clls several decrease functions which reduce the pets attributes over time
+ * When the user clicks on a button such as feed, sleep or clean then it will add values to the meter and also add 1 each time to the scoreboard
+ * If the user decides to click reset it will remove the name of the pet along with all of its values and clear it from localStorage
+*/
 function startPet() {
   if (pet.happiness === null) {
     originalValues();
@@ -146,14 +181,17 @@ function startPet() {
   });
 
   const removeNameElem = document.querySelector('#resetPet');
-  removeNameElem.addEventListener('click', removeName, updateScore);
+  removeNameElem.addEventListener('click', removeName);
 }
-// Sleep bar
+
+/** Each time the sleep button is clicked the value of the sleep meter will be set to 100
+ * The happiness meter will also be set to 100 if the sleep button is clicked
+*/
 function playing() {
   pet.sleepiness = 100;
   pet.happiness = 100;
 }
-
+/** Decreases the sleep meter over time along with the happiness meter */
 function decreaseSleep() {
   pet.sleepiness = pet.sleepiness - 0.25;
   pet.happiness = pet.happiness - 0.11;
@@ -174,17 +212,20 @@ function updateSleepMeter() {
   sleepValue.textContent = '  ' + pet.sleepiness;
 }
 
-// Hunger bar
-function feed() {
+/** Each time the hunger button is clicked the value of the hunger meter will be set to 100
+ * The happiness meter will also be set to 100 if the hunger button is clicked
+*/function feed() {
   pet.hungerness = 100;
   pet.happiness = 100;
 }
-
+/** Each time the clean button is clicked the value of the clean meter will be set to 100
+ * The happiness meter will also be set to 100 if the clean button is clicked
+*/
 function playingClean() {
   pet.cleanliness = 100;
   pet.happiness = 100;
 }
-
+/** Decreases the hunger meter over time along with the happiness meter */
 function decreaseHunger() {
   pet.hungerness = pet.hungerness - 0.25;
   pet.happiness = pet.happiness - 0.11;
@@ -198,7 +239,9 @@ function decreaseHunger() {
   updateHappyMeter();
   deathBoth();
 }
-
+/** Updates the value of the hunger meter to what the pets hunger value currently is
+ * The numerical value of the hunger meter is represented within the scoreboard
+*/
 function updateHungerMeter() {
   const hungerElem = document.querySelector('#hunger');
   hungerElem.value = pet.hungerness;
@@ -206,7 +249,7 @@ function updateHungerMeter() {
   hungerValue.textContent = '  ' + pet.hungerness;
 }
 
-
+/** Decreases the cleanliness meter over time along with the happiness meter */
 function decreaseClean() {
   pet.cleanliness = pet.cleanliness - 0.25;
   pet.happiness = pet.happiness - 0.11;
@@ -219,7 +262,9 @@ function decreaseClean() {
   adjustCleanMeters();
   updateHappyMeter();
 }
-
+/** Updates the value of the clean meter to what the pets clean value currently is
+ * The numerical value of the clean meter is represented within the scoreboard
+*/
 function adjustCleanMeters() {
   const cleanElem = document.querySelector('#clean');
   cleanElem.value = pet.cleanliness;
@@ -227,7 +272,9 @@ function adjustCleanMeters() {
   cleanValue.textContent = '  ' + pet.cleanliness;
 }
 
-
+/** Updates the value of the happiness meter to what the pets happiness value currently is which is based upon the other attributes of the pet
+ * The numerical value of the happiness meter is represented within the scoreboard
+*/
 function updateHappyMeter() {
   const happyElem = document.querySelector('#happy');
   happyElem.value = pet.happiness;
@@ -235,6 +282,8 @@ function updateHappyMeter() {
   happinessValue.textContent = '  ' + pet.happiness;
 }
 
+
+/** Hides all of the element of the eyes by selecting the html element and hiding it through the use of none */
 function hideEyes() {
   const petBlink1 = document.querySelector('#eye1');
   const petBlink2 = document.querySelector('#eye2');
@@ -245,14 +294,19 @@ function hideEyes() {
   petBlink3.style.display = 'none';
   petBlink4.style.display = 'none';
 }
-
+/** Disables the use of the buttons */
 function hideButtons() {
   document.querySelector('#hungerfeed').disabled = true;
   document.querySelector('#sleeping').disabled = true;
   document.querySelector('#cleaning').disabled = true;
   document.querySelector('#happiness').disabled = true;
 }
-
+/** If the value of the pets hunger and or the sleep value is 0 then it will do the following
+ * Clear localStorage
+ * Hide the eyes by stopping it from blinking
+ * Shows different status messsages to inform the user of the pets cause of death
+ * Stops the timer from when the pet started
+ */
 function deathBoth() {
   if (pet.hungerness === 0 && pet.sleepiness === 0) {
     localStorage.clear();
